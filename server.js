a8fd2f0f799d52f75e3d15892361f5abfe9fcf85
liushengpeng <liushengpeng@shinezone.com>
2017-12-07 11:37:33 +0800

    save

--- server.js
+++ server.js
@@ -1,31 +1,78 @@
+/**
+ * mock dataes
+ */
+
+
+let data = {
+  name: 'Owen',
+  age: 21,
+  skills: {
+    favorite: 'coding',
+    hobby: ['listen music', 'ride']
+  },
+
+  myFakeName: 'nickName',
+  nickName: 'bilibiliou',
+
+  myFirend: [{
+    name: '风清扬',
+    age: 45
+  }, {
+    name: 'pony me',
+    age: 50
+  }, {
+    name: 'robin lea',
+    age: 60
+  }],
+  foo: {
+    'abc': 123,
+    'acc': 456
+  }
+}
+
+/**  ****  ****  ****  ****  ****  ****  **/
+
 const dgram = require('dgram');
 const protobuf = require('protobufjs');
-const PORT = 8081;
+const SEND_PORT = 8081;
 
+// 建立udp连接
 let socket = dgram.createSocket({
   type: 'udp4'
 });
 
-// 和服务端逻辑相同
-let clientSchema = protobuf.loadSync('schema.proto');
-let um = clientSchema.lookupType('user_message.user');
+// 加载protobuf
+let serverSchema = protobuf.loadSync('schema.proto');
 
-socket.on('message', (buf, rinfo) => {
-  console.log('>>>>>>>>', buf);
+// 从protobuf 中抽取实例
+let um = serverSchema.lookupType('user_message.user');
 
-  let msg = um.decode(buf);
+// verify 为检测，根据schema检测数据是否有错误
+let errMsg = um.verify(data);
 
-  // 必须加上 配置信息 否则无法解析出数据
-  console.log('>>>>>>>>', um.toObject(msg, {
-    longs: String,
-    enums: String,
-    bytes: String,
-    defaults: true,
-    arrays: true,
-    objects: true,
-    oneof: true
-  }));
+if (errMsg) {
+  throw Error(errMsg);
+}
+
+// 将数据编码为二进制
+console.log(um.create(data))
+let buf = um.encode(um.create(data)).finish();
+
+socket.on('error', err => {
+  if (err) {
+    throw Error(err);
+  }
+
   socket.close();
 });
 
-socket.bind(PORT);
+
+// 发送二进制数据
+socket.send(buf, SEND_PORT, err => {
+  if (err) {
+    throw Error(err);
+  } else {
+    socket.close();
+    console.log('done');
+  }
+})
