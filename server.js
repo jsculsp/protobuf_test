84275f765a11225f42c0e3abbfc00667e6057f84
liushengpeng <liushengpeng@shinezone.com>
2017-12-07 11:35:59 +0800

    save

--- server.js
+++ server.js
@@ -1,78 +1,31 @@
-/**
- * mock dataes
- */
-
-
-let data = {
-  name: 'Owen',
-  age: 21,
-  skills: {
-    favorite: 'coding',
-    hobby: ['listen music', 'ride']
-  },
-
-  myFakeName: 'nickName',
-  nickName: 'bilibiliou',
-
-  myFirend: [{
-    name: '风清扬',
-    age: 45
-  }, {
-    name: 'pony me',
-    age: 50
-  }, {
-    name: 'robin lea',
-    age: 60
-  }],
-  foo: {
-    'abc': 123,
-    'acc': 456
-  }
-}
-
-/**  ****  ****  ****  ****  ****  ****  **/
-
 const dgram = require('dgram');
 const protobuf = require('protobufjs');
-const SEND_PORT = 8081;
+const PORT = 8081;
 
-// 建立udp连接
 let socket = dgram.createSocket({
   type: 'udp4'
 });
 
-// 加载protobuf
-let serverSchema = protobuf.loadSync('schema.proto');
+// 和服务端逻辑相同
+let clientSchema = protobuf.loadSync('schema.proto');
+let um = clientSchema.lookupType('user_message.user');
 
-// 从protobuf 中抽取实例
-let um = serverSchema.lookupType('user_message.user');
+socket.on('message', (buf, rinfo) => {
+  console.log('>>>>>>>>', buf);
 
-// verify 为检测，根据schema检测数据是否有错误
-let errMsg = um.verify(data);
+  let msg = um.decode(buf);
 
-if (errMsg) {
-  throw Error(errMsg);
-}
-
-// 将数据编码为二进制
-console.log(um.create(data))
-let buf = um.encode(um.create(data)).finish();
-
-socket.on('error', err => {
-  if (err) {
-    throw Error(err);
-  }
-
+  // 必须加上 配置信息 否则无法解析出数据
+  console.log('>>>>>>>>', um.toObject(msg, {
+    longs: String,
+    enums: String,
+    bytes: String,
+    defaults: true,
+    arrays: true,
+    objects: true,
+    oneof: true
+  }));
   socket.close();
 });
 
-
-// 发送二进制数据
-socket.send(buf, SEND_PORT, err => {
-  if (err) {
-    throw Error(err);
-  } else {
-    socket.close();
-    console.log('done');
-  }
-})
+socket.bind(PORT);
